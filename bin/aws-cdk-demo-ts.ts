a57df345f6ba489df407e018e92d1f1c98d3312f
igormgomes <igormgomes94@gmail.com>
2022-02-28 14:54:58 -0300

    feat - adding ec2 and buckets3

--- bin/aws-cdk-demo-ts.ts
+++ bin/aws-cdk-demo-ts.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AwsCdkDemoTsStack } from '../lib/aws-cdk-demo-ts-stack';
+import {AwsCdkDemoTsStack} from '../lib/aws-cdk-demo-ts-stack';
 
 const app = new cdk.App();
-new AwsCdkDemoTsStack(app, 'AwsCdkDemoTsStack');
+new AwsCdkDemoTsStack(app, 'AwsCdkDemoTsStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
--- lib/aws-cdk-demo-ts-stack.ts
+++ lib/aws-cdk-demo-ts-stack.ts
@@ -1,21 +1,45 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import {Duration, RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import {Construct} from 'constructs';
 
 export class AwsCdkDemoTsStack extends Stack {
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsCdkDemoTsQueue', {
-      visibilityTimeout: Duration.seconds(300),
-      queueName: `test`
-    });
+        const queue = new sqs.Queue(this, 'AwsCdkDemoTsQueue', {
+            visibilityTimeout: Duration.seconds(300),
+            queueName: `test`
+        });
 
-    const topic = new sns.Topic(this, 'AwsCdkDemoTsTopic');
+        const topic = new sns.Topic(this, 'AwsCdkDemoTsTopic', {
+            topicName: 'test-topic'
+        });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+        const bucket = new s3.Bucket(this, 'AwsCdkDemoTsBucket', {
+            versioned: true,
+            removalPolicy: RemovalPolicy.DESTROY
+        });
+
+        const defaultVpc = ec2.Vpc.fromLookup(this, 'vpc', {
+            isDefault: true
+        })
+
+        const lookupMachineImage = new ec2.LookupMachineImage({
+            name: 'ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20170307',
+            owners: ['099720109477']
+        });
+
+        const instance = new ec2.Instance(this, 'AwsCdkDemoTsEc2', {
+            machineImage: lookupMachineImage,
+            vpc: defaultVpc,
+            instanceType: new ec2.InstanceType('t2.micro')
+        })
+
+        topic.addSubscription(new subs.SqsSubscription(queue));
+    }
 }
